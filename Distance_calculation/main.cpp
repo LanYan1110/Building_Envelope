db1b1750a4985debd7dd5a5e486eb999d2fe1078
LanYan1110 <92783160+LanYan1110@users.noreply.github.com>
2023-03-17 11:40:42 +0100

    _sample_points_Using_CGAL

--- Distance_calculation/main.cpp
+++ Distance_calculation/main.cpp
@@ -56,42 +56,56 @@ std::vector<std::string> GetInputs(std::string dir) {
 
 
 int main(){
-    std::string s_reconstructed_obj="C:/Users/seuya/Documents/Thesis/distance_calculation/BIMobject_Demo_Model_Revit.xyz";
+    // std::string s_reconstructed_obj="C:/Users/seuya/Documents/Thesis/distance_calculation/BIMobject_Demo_Model_Revit.xyz";
     std::string reconstructed_obj="C:/Users/seuya/Documents/Thesis/distance_calculation/BIMobject_Demo_Model_Revit.ifc_0.5off.obj";
-    std::string original_obj="C:/Users/seuya/Documents/Thesis/distance_calculation/BIMobject_Demo_Model_Revit.obj";
+    std::string original_obj="C:/Users/seuya/Documents/Thesis/distance_calculation/BIMobject_Demo_Model_Revit.ifc_0.5off.obj";
     std::string distances="C:/Users/seuya/Documents/Thesis/distance_calculation/"+clear_slash(original_obj)+"distances.txt";
+    std::string sampled_points="C:/Users/seuya/Documents/Thesis/distance_calculation/"+clear_slash(original_obj)+"sampled_points.xyz";
     std::vector<Point> r_points;
     std::vector<std::vector<std::size_t>> r_faces2;
     
-    //// read polygon soup
-    //if(!CGAL::IO::read_polygon_soup(reconstructed_obj, r_points, r_faces2) ||
-    //r_points.size() == 0 || r_faces2.size() == 0){
-    //    std::cerr << "Error: can not read input file.\n";
-    //    return 1;
-    //}
+    // read polygon soup
+    if(!CGAL::IO::read_polygon_soup(reconstructed_obj, r_points, r_faces2) ||
+    r_points.size() == 0 || r_faces2.size() == 0){
+       std::cerr << "Error: can not read input file.\n";
+       return 1;
+    }
 
-    //// orient polygon soup
-    //PMP::orient_polygon_soup(r_points, r_faces2);
-    //
-    //// convert to polygon mesh
-    //Mesh r_surface_mesh;
-    //if(PMP::is_polygon_soup_a_polygon_mesh(r_faces2)){
-    //    PMP::polygon_soup_to_polygon_mesh(r_points, r_faces2, r_surface_mesh);
-    //}
+    // orient polygon soup
+    PMP::orient_polygon_soup(r_points, r_faces2);
+    
+    // convert to polygon mesh
+    Mesh r_surface_mesh;
+    if(PMP::is_polygon_soup_a_polygon_mesh(r_faces2)){
+       PMP::polygon_soup_to_polygon_mesh(r_points, r_faces2, r_surface_mesh);
+    }
 
+    
 
     // sample the points
     std::vector<Point> r_sample_points;
-    //r_sample_points.push_back(Point(0,0,0));
-    //open file
-    std::ifstream input(s_reconstructed_obj);
-     double x, y, z;
-	 for (int i = 0; i <4838; ++i){
-	 	input >> x >> y >> z;
-	 	r_sample_points.push_back(Point(x, y, z));
-	 }
+    //std::OutputIterator outputIterator(r_sample_points);
+    // construct output iterator for the point vector
+    std::back_insert_iterator<std::vector<Point>> outputIterator(r_sample_points);
     
-    //PMP::sample_triangle_mesh(r_surface_mesh,r_sample_points);
+    r_sample_points.push_back(Point(0,0,0));
+    //open file
+    // std::ifstream input(s_reconstructed_obj);
+    //  double x, y, z;
+	//  for (int i = 0; i <4838; ++i){
+	//  	input >> x >> y >> z;
+	//  	r_sample_points.push_back(Point(x, y, z));
+	//  }
+    
+    PMP::triangulate_faces(r_surface_mesh);
+    PMP::sample_triangle_mesh(r_surface_mesh,outputIterator);
+
+    // write the sampled points to a file
+    std::ofstream out(sampled_points);
+    for (auto p: r_sample_points){
+        out << p.x() << " " << p.y() << " " << p.z() << std::endl;
+    }
+    out.close();
 
     std::vector<Point> points;
     std::vector<std::vector<std::size_t>> faces1;
@@ -114,6 +128,7 @@ int main(){
     Tree tree;
     tree.insert(faces(surface_mesh).first, faces(surface_mesh).second, surface_mesh);
     tree.build(); // CGAL_assertion( idx < data.size() ) fails
+
 
     //time the process
     auto start = std::chrono::high_resolution_clock::now();
